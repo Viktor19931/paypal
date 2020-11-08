58747b7fa44f993fdba4972d23a3d5ae5255312d
Viktor <viktor_hordubey@yahoo.com>
2020-11-08 13:46:13 +0200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import Button from "./Button";
 
 const data = [
   { email: "vhardubej51@yahoo.com", id: "UQ7ZZP8XYF7PS" },
-  { email: "vhardubej53@yahoo.com", id: "H3GDFVSG8N8K4" },
-  { email: "vhardubej53@yahoo.com", id: "H3GDFVSG8N8K4" },
+  { email: "vhardubej53@yahoo.com", id: "H3GDFVSG8N8K4" }
 ];
 
 const App: FC = () => {
